951d95363ae91b6fc34043d717428bf818349e32
NickShumar <nshumar@mitre.org>
2026-05-27 14:07:24 -0400

    updated from target_config to unpack_target_from_uri
    Signed-off-by: NickShumar <nshumar@mitre.org>

--- lib/train-k8s-container/connection.rb
+++ lib/train-k8s-container/connection.rb
@@ -19,7 +19,7 @@ module TrainPlugins
       # @example k8s-container://default/shell-demo/nginx
 
       def initialize(options)
-        options = Train.target_config(options) if options[:target]
+        options = Train.unpack_target_from_uri(options) if options[:target]
         super
 
         # Parse URI components:
